4c0f54733f000085896093a9233b5c81dacfefbd
Bohdan <kinashbohdan555@gmail.com>
2026-09-04 22:25:54 +0300

    add two sum solution

---
+++ leetcode/two-sum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    vector<int> res = {i,j};
+                    return res;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> nums{5, 6, 3, 7};
+    int target = 8;
+    sol.twoSum(nums, target);
+    return 0;
+}
